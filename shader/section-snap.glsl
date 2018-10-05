7198a764cda98d55b9ffae9aae12a9b65448af70
dy <df.creative@gmail.com>
2018-10-05 23:10:27 +0300

    Fix symmetrical patterns

--- shader/section-snap.glsl
+++ shader/section-snap.glsl
@@ -2,20 +2,22 @@ precision highp float;
 
 // turn, or 2 * pi
 const float TAU = 6.283185307179586;
+const float PI = 3.141592653589793;
+
+float round (float v) {
+	return floor(v + .5);
+}
 
 // snap tangent to angle section
 vec2 snap(vec2 tangent, float sections) {
 	float angle = atan(tangent.y, tangent.x);
+
 	float step = TAU / sections;
 
-	angle = floor(angle / step) * step;
+	angle = round(angle / step) * step;
 
-	// shift section by half
-	angle += TAU * .5 / sections;
-
-	if (angle > TAU) angle -= TAU;
-	if (angle < 0.) angle += TAU;
-
+	// exploit symmetrical pattern alignment
+	angle = mod(angle, PI);
 
 	return vec2(cos(angle), sin(angle));
 }
