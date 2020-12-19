5d544746bbbcf09223281a0da5549a485782ffe1
Ruijin Wu <cszwrj@gmail.com>
2020-12-19 14:48:59 -0800

    handle hole, add 2 testes

--- index.js
+++ index.js
@@ -516,20 +516,43 @@ Line2D.prototype.update = function (options) {
 					pos[ptr++] = y
 				}
 
+				// use "ids" to track the boundary of segment
+				// the keys in "ids" is the end boundary of a segment, or split point
 				if(!(state.count-1 in ids))
-					ids[state.count] = state.count-1   // make sure there is at least polygon
+					ids[state.count] = state.count-1   // make sure there is at least one segment
 					
-				let splits = Object.keys(ids).map(e => e|0).sort(function(a, b){return a - b})
+				let splits = Object.keys(ids).map(Number).sort(function(a, b){return a - b})
+					
 				
 				let split_triangles = []
 				let base = 0
 				
+				// do not split holes
+				let hole_base = state.hole != null ? state.hole[0] : null
+				if(hole_base != null){
+					let last_id = splits.findIndex((e)=>e>=hole_base)
+					splits = splits.slice(0,last_id)
+					splits.push(hole_base)
+				}
+				
 				for (let i = 0; i < splits.length; i++)
 				{
-					let triangles = triangulate(pos.slice(base*2, splits[i]*2), state.hole || [])
-					for (let e = 0; e < triangles.length; e++)
-						split_triangles.push(triangles[e] + base)
+					// create temporary pos array with only one segment and all the holes
+					let seg_pos = [].concat(
+						pos.slice(base*2, splits[i]*2), 
+						hole_base?pos.slice(hole_base*2):[]
+					)
+					let hole = (state.hole || []).map((e)=>e-hole_base+(splits[i]-base))
+					let triangles = triangulate(seg_pos, hole)
+					// map triangle index back to the original pos buffer
+					triangles = triangles.map(
+						(e)=>(e < (splits[i] - base)) ? e + base : e - (splits[i] - base) + hole_base
+					)
+					split_triangles.push(...triangles)
 					base = splits[i] + 1 // skip split point
+				}
+				for (let i = 0, l = split_triangles.length; i < l; i++) {
+					if (ids[split_triangles[i]] != null) split_triangles[i] = ids[split_triangles[i]]
 				}
 
 				state.triangles = split_triangles
